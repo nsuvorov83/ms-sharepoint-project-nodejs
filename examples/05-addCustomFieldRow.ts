0b503073066bb8b60597dcbe2a8fd062384d2e62
Nikolay Suvorov <nikolay.suvorov@group.omd.ru>
2018-01-23 16:41:19 +0300

    Auto getting index of new row added

--- examples/05-addCustomFieldRow.ts
+++ examples/05-addCustomFieldRow.ts
@@ -6,7 +6,7 @@ const settings = require(path.join(__dirname, '../config/private.json'));
 declare const PS: any;
 
 let jsomNodeOptions: IJsomNodeInitSettings = {
-  siteUrl: settings.siteUrl,
+  siteUrl: settings.msProjectUrl,
   authOptions: { ...settings },
   modules: [ 'project' ]
 };
@@ -21,18 +21,25 @@ let jsomNodeOptions: IJsomNodeInitSettings = {
   const myLut = lookupTables.getByGuid('ebb134c6-29be-e711-80d3-00155da4760e');
 
   const myEntries = myLut.get_entries();
+  projContext.load(myEntries);
+
+  //Отрабатываем промежуточные запросы
+  await projContext.executeQueryPromise();
+
+  //Определяем индекс новой записи и её GUID
+  let new_index = myEntries.get_count() + 1;
 
   const newId = SP.Guid.newGuid();
 
   const lutEntry = new PS.LookupEntryCreationInformation();
 
-  lutEntry.set_description('my descr');
-  lutEntry.set_sortIndex(11); //Index of new row (previosly get it see ex. 04-getCustomRows.ts)
+  lutEntry.set_description('my descr22');
+  lutEntry.set_sortIndex(new_index); //Index of new row (previosly get it see ex. 04-getCustomRows.ts)
   lutEntry.set_id(newId);
   lutEntry.set_parentId(null);
 
   const lutValue = new PS.LookupEntryValue();
-  lutValue.set_textValue('test');
+  lutValue.set_textValue('test22');
   lutEntry.set_value(lutValue);
   myEntries.add(lutEntry);
 
